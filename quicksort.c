d45bda651212e87b9a121f97a9786372a70ff682
gremble0 <hermanstornes@gmail.com>
2023-12-26 21:56:25 +0100

    Almost working quicksort

--- quicksort.c
+++ quicksort.c
@@ -1,14 +1,22 @@
+#include <stdio.h>
+
 #include "quicksort.h"
 
 void quicksort(int buf[], int start, int end) {
     // basecase
-    // if (end == 0) {
-    //     return;
-    // }
+    if (start >= end) {
+        return;
+    }
 
     // int pivot = (buf[0] + buf[end - 1]) / 2;
     // int pivot = buf[end / 2];
     int pivot = buf[end - 1];
+
+    printf("start: %d, end: %d\n", start, end);
+    for (int i = 0; i < 7; i++) {
+        printf("%d: %d, ", i, buf[i]);
+    }
+    printf("\n");
 
     int left_i = 0, right_i = end - 1;
     // loop up until the second to last element in array
@@ -25,6 +33,8 @@ void quicksort(int buf[], int start, int end) {
         quicksort(buf, start, end);
     } else {
         swap(buf, left_i, end - 1); // swap left with pivot
+        quicksort(buf, start, left_i);
+        quicksort(buf, left_i, end);
     }
 }
 
