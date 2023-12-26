f5a377dd9808a238ac139319724d0db689b3b9c2
gremble0 <hermanstornes@gmail.com>
2023-12-26 21:37:13 +0100

    Add swap function for quicksort

--- quicksort.c
+++ quicksort.c
@@ -1,5 +1,35 @@
-#include <stdio.h>
+#include "quicksort.h"
 
-void quicksort(int buf[], int bufsize) {
-    printf("%d\n", bufsize);
+void quicksort(int buf[], int start, int end) {
+    // basecase
+    // if (end == 0) {
+    //     return;
+    // }
+
+    // int pivot = (buf[0] + buf[end - 1]) / 2;
+    // int pivot = buf[end / 2];
+    int pivot = buf[end - 1];
+
+    int left_i = 0, right_i = end - 1;
+    // loop up until the second to last element in array
+    while (buf[left_i] < pivot) {
+        left_i++;
+    }
+
+    while (buf[right_i] > pivot) {
+        right_i++;
+    }
+
+    if (left_i < right_i) {
+        swap(buf, left_i, right_i);
+        quicksort(buf, start, end);
+    } else {
+        swap(buf, left_i, end - 1); // swap left with pivot
+    }
+}
+
+void swap(int buf[], int i, int j) {
+    int at_i = buf[i];
+    buf[i] = buf[j];
+    buf[j] = at_i;
 }
--- quicksort.h
+++ quicksort.h
@@ -1,1 +1,2 @@
-void quicksort(int buf[], int bufsize);
+void quicksort(int buf[], int start, int end);
+void swap(int buf[], int i, int j);
