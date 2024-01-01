ec986efe1f7187c1c7bd6fc3b81ff616b954c1d8
gremble0 <hermanstornes@gmail.com>
2024-01-01 20:50:49 +0100

    Add arr_reverse function

--- array.c
+++ array.c
@@ -2,6 +2,18 @@
 
 #include "array.h"
 
+void arr_swap(int buf[], int i, int j) {
+    int at_i = buf[i];
+    buf[i] = buf[j];
+    buf[j] = at_i;
+}
+
+void arr_reverse(void *arr, size_t arr_size) {
+    for (size_t i = 0; i < arr_size / 2; ++i) {
+        arr_swap(arr, i, arr_size - 1 - i);
+    }
+}
+
 int arr_contains(void *arr, size_t arr_size, void *val, size_t val_type_size) {
     // Cast void* to single byte pointers so we can multiply it by val_type_size
     char *a = (char*)arr;
