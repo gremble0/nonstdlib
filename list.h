6d02bb0a89ae91424856e74177be9ff637b30bae
gremble0 <hermanstornes@gmail.com>
2024-01-01 03:01:37 +0100

    Add interface for list data structure

---
+++ list.h
@@ -0,0 +1,12 @@
+typedef struct list_t {
+    void *items;
+    int max_size;
+    int cur_size;
+} list_t;
+
+void list_reverse(list_t *arr);
+void list_clear(list_t *arr);
+void list_append(list_t *arr, void *val);
+void list_remove(list_t *arr, void *val);
+int list_contains(list_t *arr, void *val);
+void *list_get(list_t *arr, int index);
