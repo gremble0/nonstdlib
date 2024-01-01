e41daaa9be1d84e86200c7e241eff494cf6bf4ce
gremble0 <hermanstornes@gmail.com>
2024-01-01 04:16:38 +0100

    Rename list parameter

--- list.h
+++ list.h
@@ -4,9 +4,10 @@ typedef struct list_t {
     int cur_size;
 } list_t;
 
-void  list_reverse(list_t *arr);
-void  list_clear(list_t *arr);
-void  list_append(list_t *arr, void *val);
-void  list_remove(list_t *arr, void *val);
-int   list_contains(list_t *arr, void *val);
-void *list_get(list_t *arr, int index);
+void  list_reverse(list_t *list);
+void  list_clear(list_t *list);
+void  list_append(list_t *list, void *val);
+void  list_remove(list_t *list, void *val);
+void *list_pop(list_t *list);
+int   list_contains(list_t *list, void *val);
+void *list_get(list_t *list, int index);
