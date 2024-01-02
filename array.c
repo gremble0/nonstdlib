1c5c287679a2c30cb00ab63b0ea918cf140d3969
gremble0 <hermanstornes@gmail.com>
2024-01-02 20:54:37 +0100

    Add docstrings in array.c

--- array.c
+++ array.c
@@ -2,6 +2,12 @@
 
 #include "array.h"
 
+/**
+ * @param arr           array to swap elements in
+ * @param i             index into arr of first element to swap
+ * @param j             index into arr of second element to swap
+ * @param arr_type_size amount of bytes used for each element in arr
+ */
 void arr_swap(void *arr, int i, int j, size_t arr_type_size) {
     char *a = (char*)arr;
     char temp[arr_type_size];
@@ -15,12 +21,23 @@ void arr_swap(void *arr, int i, int j, size_t arr_type_size) {
     memcpy(a + j * arr_type_size, temp, arr_type_size);
 }
 
+/**
+ * @param arr           array to reverse
+ * @param arr_size      size of arr
+ * @param arr_type_size amount of bytes used for each element in arr
+ */
 void arr_reverse(void *arr, size_t arr_size, size_t arr_type_size) {
     for (size_t i = 0; i < arr_size / 2; ++i) {
         arr_swap(arr, i, arr_size - 1 - i, arr_type_size);
     }
 }
 
+/**
+ * @param arr           array to check in
+ * @param arr_size      size of arr
+ * @param val           value to check if exists in arr
+ * @param arr_type_size amount of bytes used for each element in arr
+ */
 int arr_contains(void *arr, size_t arr_size, void *val, size_t arr_type_size) {
     // Cast void* to single byte pointers so we can multiply it by val_type_size
     char *a = (char*)arr;
