cbfa5515e2640fe2a1800a91dbfcedd53ac58546
gremble0 <hermanstornes@gmail.com>
2024-01-08 05:28:48 +0100

    Remove unnecessary casts and change test Makefile

--- src/array.c
+++ src/array.c
@@ -9,16 +9,15 @@
  * @param arr_type_size amount of bytes used for each element in arr
  */
 void arr_swap(void *arr, int i, int j, size_t arr_type_size) {
-    char *a = (char*)arr;
     char temp[arr_type_size];
 
     // TODO: maybe do this ourselves instead of using string.h
     // Copy contents of a[i] into temporary buffer
-    memcpy(temp, a + i * arr_type_size, arr_type_size);
+    memcpy(temp, arr + i * arr_type_size, arr_type_size);
     // Copy a[j] into a[i]
-    memcpy(a + i * arr_type_size, a + j * arr_type_size, arr_type_size);
+    memcpy(arr + i * arr_type_size, arr + j * arr_type_size, arr_type_size);
     // Copy contents of temp into a[j]
-    memcpy(a + j * arr_type_size, temp, arr_type_size);
+    memcpy(arr + j * arr_type_size, temp, arr_type_size);
 }
 
 /**
@@ -40,15 +39,11 @@ void arr_reverse(void *arr, size_t arr_size, size_t arr_type_size) {
  * @return              1 if arr contains val 0 if not
  */
 int arr_contains(void *arr, size_t arr_size, void *val, size_t arr_type_size) {
-    // Cast void* to single byte pointers so we can multiply it by val_type_size
-    char *a = (char*)arr;
-    char *v = (char*)val;
-
     for (size_t i = 0; i < arr_size; ++i) {
         // TODO: maybe do this ourselves instead of using string.h
         // Move i * val_type_size and compare val_type_size bytes each iteration
         // We have to do it like this because "generics" in C is quite janky
-        if (memcmp(&a[i * arr_type_size], v, arr_type_size) == 0) {
+        if (memcmp(&arr[i * arr_type_size], val, arr_type_size) == 0) {
             return 1;
         }
     }
