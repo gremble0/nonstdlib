484d24a339effffb86b7596b0e87ec17442d44de
gremble0 <hermanstornes@gmail.com>
2024-01-19 15:56:37 +0100

    Move list_contains test to separate function

--- test/test_list.c
+++ test/test_list.c
@@ -33,34 +33,30 @@ void test_list_append(void) {
     assert(memcmp(&a, list_get(list2, 1), list2->type_size) != 0);
     assert(memcmp(&b, list_get(list2, 1), list2->type_size) == 0);
 
+    list_free(list);
     printf("\033[0;32mAll list_append tests passed\033[0;37m\n");
+}
+
+void test_list_contains(void) {
+    list_t *list = list_init(5, sizeof(char*));
+
+    const int a = 2;
+    const int b = 44;
+
+    list_append(list, &a);
+    list_append(list, &b);
+    
+    assert(list_contains(list, &a) == 1);
+    assert(list_contains(list, &b) == 1);
+
+    list_free(list);
+    printf("\033[0;32mAll list tests passed\033[0;37m\n");
 }
 
 void test_list(void) {
     printf("Testing list data structure...\n");
     test_list_append();
-
-    list_t *list = list_init(5, sizeof(char*));
-
-    list_append(list, "a");
-    list_append(list, "asd");
-    assert(strcmp("a", list_get(list, 0)) == 0);
-    assert(strcmp("asd", list_get(list, 1)) == 0);
-    assert(list->cur_size == 2);
-
-    list_clear(list);
-    list_append(list, "b");
-    assert(strcmp("b", list_get(list, 0)) == 0);
-    assert(list->cur_size == 1);
-
-    const int a = 2;
-    const long long b = 44;
-    assert(list_contains(list, "a") == 0);
-    assert(list_contains(list, "b") == 1);
-    assert(list_contains(list, &a) == 0);
-    assert(list_contains(list, &b) == 0);
-
-    list_free(list);
+    test_list_contains();
 
     printf("\033[0;32mAll list tests passed\033[0;37m\n");
 }
