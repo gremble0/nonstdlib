c7edbe4e178dc96f8c373c198422ee31ab29cbe0
gremble0 <hermanstornes@gmail.com>
2024-01-20 11:29:48 +0100

    Add test_ll_append function

--- test/test_linked_list.c
+++ test/test_linked_list.c
@@ -5,20 +5,20 @@
 
 #include "linked_list.h"
 
-void test_ll_push(void) {
+void test_ll_append(void) {
     ll_t *ll = ll_init(sizeof(char *));
 
-    ll_push(ll, "a");
-    ll_push(ll, "b");
-    ll_push(ll, "c");
+    ll_append(ll, "a");
+    ll_append(ll, "b");
+    ll_append(ll, "c");
 
     assert(ll->cur_size == 3);
-    assert(strcmp(ll_peek(ll), "c") == 0);
+    assert(strcmp(ll_peek(ll), "a") == 0);
     assert(strcmp(ll_seek(ll, 1), "b") == 0);
-    assert(strcmp(ll_seek(ll, 2), "a") == 0);
+    assert(strcmp(ll_seek(ll, 2), "c") == 0);
 
     ll_free(ll);
-    printf("\033[0;32mTests for ll_push passed\033[0;37m\n");
+    printf("\033[0;32mTests for ll_append passed\033[0;37m\n");
 }
 
 void test_ll_pop(void) {
@@ -45,9 +45,26 @@ void test_ll_pop(void) {
     printf("\033[0;32mTests for ll_pop passed\033[0;37m\n");
 }
 
+void test_ll_push(void) {
+    ll_t *ll = ll_init(sizeof(char *));
+
+    ll_push(ll, "a");
+    ll_push(ll, "b");
+    ll_push(ll, "c");
+
+    assert(ll->cur_size == 3);
+    assert(strcmp(ll_peek(ll), "c") == 0);
+    assert(strcmp(ll_seek(ll, 1), "b") == 0);
+    assert(strcmp(ll_seek(ll, 2), "a") == 0);
+
+    ll_free(ll);
+    printf("\033[0;32mTests for ll_push passed\033[0;37m\n");
+}
+
 void test_linked_list(void) {
     printf("Testing linked list...\n");
-    test_ll_push();
+    test_ll_append();
     test_ll_pop();
+    test_ll_push();
     printf("\033[0;32mAll linked list tests passed\033[0;37m\n");
 }
