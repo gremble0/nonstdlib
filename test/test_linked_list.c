e6b8e21fd6fcd10d2d53bf4080ac440fb3b007e9
gremble0 <hermanstornes@gmail.com>
2024-02-28 02:26:49 +0100

    Add ll_peek_front, ll_peek_back

--- test/test_linked_list.c
+++ test/test_linked_list.c
@@ -14,17 +14,21 @@ void test_ll_push_back_seek(void) {
   ll_push_back(ll, "d");
   ll_push_back(ll, "e");
   ll_push_back(ll, "f");
+  // ll should be: "a" -> "b" -> "c" -> "d" -> "e" -> "f"
 
+  // order of peeking and seeking should not matter
   assert(ll->cur_size == 6);
-  assert(strcmp(ll_peek(ll), "a") == 0);
+  assert(strcmp(ll_peek_front(ll), "a") == 0);
   assert(strcmp(ll_seek(ll, 5), "f") == 0);
   assert(strcmp(ll_seek(ll, 2), "c") == 0);
+  assert(strcmp(ll_peek_back(ll), "f") == 0);
   assert(strcmp(ll_seek(ll, 1), "b") == 0);
   assert(strcmp(ll_seek(ll, 3), "d") == 0);
   assert(strcmp(ll_seek(ll, 4), "e") == 0);
 
   ll_free(ll);
-  printf("\033[0;32mTests for ll_push_back and ll_seek passed\033[0;37m\n");
+  printf("\033[0;32mTests for ll_push_back, ll_seek, ll_peek(front & back) "
+         "passed\033[0;37m\n");
 }
 
 void test_ll_push_front_pop(void) {
@@ -56,6 +60,28 @@ void test_ll_push_front_pop(void) {
          "passed\033[0;37m\n");
 }
 
+void test_ll_reverse(void) {
+  ll_t *ll = ll_init(sizeof(char *));
+
+  // reversing empty ll should not error
+  ll_reverse(ll);
+
+  ll_push_front(ll, "a");
+  ll_push_front(ll, "b");
+  ll_push_back(ll, "c");
+  // ll should be: "b" -> "a" -> "c"
+
+  ll_reverse(ll);
+  // ll should be: "c" -> "a" -> "b"
+
+  assert(strcmp(ll_peek_front(ll), "c") == 0);
+  assert(strcmp(ll_seek(ll, 1), "a") == 0);
+  assert(strcmp(ll_peek_back(ll), "b") == 0);
+
+  ll_free(ll);
+  printf("\033[0;32mTests for ll_reverse passed\033[0;37m\n");
+}
+
 void test_ll_all(void) {
   ll_t *ll = ll_init(sizeof(char *));
 
@@ -66,27 +92,29 @@ void test_ll_all(void) {
 
   assert(ll->cur_size == 3);
   assert(strcmp(ll_seek(ll, 2), "c") == 0);
-  assert(strcmp(ll_peek(ll), "b") == 0);
+  assert(strcmp(ll_peek_front(ll), "b") == 0);
 
   ll_pop_back(ll);
   // ll should be: "b" -> "a"
-  assert(strcmp(ll_peek(ll), "b") == 0);
+  assert(strcmp(ll_peek_front(ll), "b") == 0);
   assert(strcmp(ll_seek(ll, 1), "a") == 0);
   ll_pop_back(ll);
   // ll should be: "b"
-  assert(strcmp(ll_peek(ll), "b") == 0);
+  assert(strcmp(ll_peek_front(ll), "b") == 0);
   ll_pop_back(ll);
   // ll should be: (nil)
-  assert(ll_peek(ll) == NULL);
+  assert(ll_peek_front(ll) == NULL);
 
   ll_free(ll);
-  printf("\033[0;32mTests for all linked list functions passed\033[0;37m\n");
+  printf("\033[0;32mTests for mix of all linked list functions "
+         "passed\033[0;37m\n");
 }
 
 void test_linked_list(void) {
   printf("Testing linked list...\n");
   test_ll_push_back_seek();
   test_ll_push_front_pop();
+  test_ll_reverse();
   test_ll_all();
   printf("\033[0;32mAll linked list tests passed\033[0;37m\n");
 }
