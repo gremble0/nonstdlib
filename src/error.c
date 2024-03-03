c0e5d4b53d8dcb7463243a4db2924c04f998ef84
gremble0 <hermanstornes@gmail.com>
2024-03-03 10:17:26 +0100

    add err_custom()

--- src/error.c
+++ src/error.c
@@ -1,4 +1,5 @@
 #include <execinfo.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -17,33 +18,32 @@ static void err_print_stacktrace() {
   free(strs);
 }
 
-_Noreturn void err_io_fail(const char *file_path) {
+_Noreturn void err_custom(const char *errormsg, ...) {
   err_print_stacktrace();
-  fprintf(stderr, "ERROR_IO_FAIL: error doing IO to file at '%s'\n", file_path);
+
+  va_list args;
+  va_start(args, errormsg);
+  vfprintf(stderr, errormsg, args);
+  va_end(args);
 
   exit(1);
+}
+
+_Noreturn void err_io_fail(const char *file_path) {
+  err_custom("ERROR_IO_FAIL: error doing IO to file at '%s'\n", file_path);
 }
 
 _Noreturn void err_index_out_of_bounds(size_t index, size_t max_index) {
-  err_print_stacktrace();
-  fprintf(stderr,
-          "ERROR_INDEX_OUT_OF_BOUNDS: index %zu out of bounds where max_index "
-          "is %zu\n",
-          index, max_index);
-
-  exit(1);
+  err_custom(
+      "ERROR_INDEX_OUT_OF_BOUNDS: index %zu out of bounds where max_index "
+      "is %zu\n",
+      index, max_index);
 }
 
 _Noreturn void err_malloc_fail() {
-  err_print_stacktrace();
-  fprintf(stderr, "ERROR_MALLOC_FAIL: Failed to allocate memory\n");
-
-  exit(1);
+  err_custom("ERROR_MALLOC_FAIL: Failed to allocate memory\n");
 }
 
 _Noreturn void err_pop_from_empty_list() {
-  err_print_stacktrace();
-  fprintf(stderr, "ERROR_POP_FROM_EMPTY_LIST: Cannot pop from empty list\n");
-
-  exit(1);
+  err_custom("ERROR_POP_FROM_EMPTY_LIST: Cannot pop from empty list\n");
 }
--- src/error.h
+++ src/error.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 #include <stdnoreturn.h>
 
+_Noreturn void err_custom(const char *errmsg, ...);
 _Noreturn void err_index_out_of_bounds(size_t index, size_t max_index);
 _Noreturn void err_io_fail(const char *file_path);
 _Noreturn void err_malloc_fail();
