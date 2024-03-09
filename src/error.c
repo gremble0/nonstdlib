3efb235113fb14124abce471728deb9793675000
gremble0 <hermanstornes@gmail.com>
2024-03-09 12:56:05 +0100

    Reworked error api

--- src/error.c
+++ src/error.c
@@ -1,11 +1,12 @@
+#include <errno.h>
 #include <execinfo.h>
-#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "error.h"
 
-static void err_print_stacktrace() {
+void err_print_stacktrace() {
   void *callstack[128];
   int frames = backtrace(callstack, 128);
   char **strs = backtrace_symbols(callstack, frames);
@@ -15,35 +16,38 @@ static void err_print_stacktrace() {
     fprintf(stderr, "%s\n", strs[i]);
   }
 
+  if (errno != 0) {
+    fprintf(stderr, "Detected non zero errno(%d): %s\n", errno,
+            strerror(errno));
+  }
+
   free(strs);
 }
 
-_Noreturn void err_custom(const char *errormsg, ...) {
+_Noreturn void err_io_fail(const char *file_path) {
   err_print_stacktrace();
-
-  va_list args;
-  va_start(args, errormsg);
-  vfprintf(stderr, errormsg, args);
-  va_end(args);
-
+  fprintf(stderr, "ERROR_IO_FAIL: IO operation to file '%s' failed\n",
+          file_path);
   exit(1);
 }
 
-_Noreturn void err_io_fail(const char *file_path) {
-  err_custom("ERROR_IO_FAIL: error doing IO to file at '%s'\n", file_path);
-}
-
 _Noreturn void err_index_out_of_bounds(size_t index, size_t max_index) {
-  err_custom(
-      "ERROR_INDEX_OUT_OF_BOUNDS: index %zu out of bounds where max_index "
-      "is %zu\n",
-      index, max_index);
+  err_print_stacktrace();
+  fprintf(stderr,
+          "ERROR_INDEX_OUT_OF_BOUNDS: index %zu out of bounds where max_index "
+          "is %zu\n",
+          index, max_index);
+  exit(1);
 }
 
 _Noreturn void err_malloc_fail() {
-  err_custom("ERROR_MALLOC_FAIL: Failed to allocate memory\n");
+  err_print_stacktrace();
+  fprintf(stderr, "ERROR_MALLOC_FAIL: Failed to allocate memory\n");
+  exit(1);
 }
 
 _Noreturn void err_pop_from_empty_list() {
-  err_custom("ERROR_POP_FROM_EMPTY_LIST: Cannot pop from empty list\n");
+  err_print_stacktrace();
+  fprintf(stderr, "ERROR_POP_FROM_EMPTY_LIST: Cannot pop from empty list\n");
+  exit(1);
 }
