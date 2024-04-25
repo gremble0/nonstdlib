c039498fbc4da0f0dc5bdbc1159c5d7b5e5e07e8
gremble0 <hermanstornes@gmail.com>
2024-04-25 08:09:46 +0200

    Start string module

---
+++ src/nstring.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "nerror.h"
+#include "nstring.h"
+
+string_t *string_of(const char *s, size_t s_len) {
+  string_t *str = malloc(s_len * sizeof(char));
+  if (str == NULL)
+    err_malloc_fail();
+
+  memcpy(str->s, s, s_len);
+  str->len = s_len;
+
+  return str;
+}
+
+void string_clear(string_t *str) {
+  str->s = NULL;
+  str->len = 0;
+}
+
+void string_set(string_t *str, const char *s, size_t s_len) {
+  str->s = realloc(str->s, s_len * sizeof(char));
+  if (str->s == NULL)
+    err_malloc_fail();
+
+  memcpy(str->s, s, s_len);
+}
+
+void string_free(string_t *string) {
+  free(string->s);
+  free(string);
+}
---
+++ src/nstring.h
@@ -0,0 +1,11 @@
+#include <stddef.h>
+
+typedef struct {
+  char *s; // Should not be NULL terminated
+  size_t len;
+} string_t;
+
+string_t *string_of(const char *s, size_t s_len);
+void string_clear(string_t *str);
+void string_free(string_t *str);
+void string_set(string_t *str, const char *s, size_t s_len);
