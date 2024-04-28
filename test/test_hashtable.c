69ce24cbf0ea99e3f4894dba78e82620548530d5
gremble0 <hermanstornes@gmail.com>
2024-04-28 12:49:41 +0200

    Remove unnecessary strlen calls

--- test/test_hashtable.c
+++ test/test_hashtable.c
@@ -11,16 +11,18 @@ void test_hashtable_int(void) {
   int a = 4;
   int b = 0;
   int c = 9;
+  char asd[] = "asd";
+  char bbb[] = "bbb";
 
-  ht_put(table, "asd", strlen("asd") + 1, &a);
-  DEBUG_ASSERT(table->n_entries == 1);
-  ht_put(table, "bbb", strlen("bbb") + 1, &b);
-  DEBUG_ASSERT(table->n_entries == 2);
-  ht_put(table, "bbb", strlen("bbb") + 1, &c);
-  DEBUG_ASSERT(table->n_entries == 2);
+  ht_put(table, asd, sizeof(asd), &a);
+  DEBUG_ASSERT(table->cur_size == 1);
+  ht_put(table, bbb, sizeof(bbb), &b);
+  DEBUG_ASSERT(table->cur_size == 2);
+  ht_put(table, bbb, sizeof(bbb), &c);
+  DEBUG_ASSERT(table->cur_size == 2);
 
-  DEBUG_ASSERT(*(int *)ht_get(table, "asd", strlen("asd") + 1) == 4);
-  DEBUG_ASSERT(*(int *)ht_get(table, "bbb", strlen("bbb") + 1) == 9);
+  DEBUG_ASSERT(*(int *)ht_get(table, "asd", sizeof(asd)) == 4);
+  DEBUG_ASSERT(*(int *)ht_get(table, "bbb", sizeof(bbb)) == 9);
 
   ht_free(table);
 }
@@ -28,35 +30,37 @@ void test_hashtable_int(void) {
 void test_hashtable_string(void) {
   ht_t *table = ht_init(5);
 
-  ht_put(table, "asd", strlen("asd") + 1, "YOYO");
-  DEBUG_ASSERT(table->n_entries == 1);
-  ht_put(table, "asd", strlen("asd") + 1, "YAYA");
-  DEBUG_ASSERT(table->n_entries == 1);
-  ht_put(table, "qwerty", strlen("qwerty") + 1, "123");
-  DEBUG_ASSERT(table->n_entries == 2);
-  ht_put(table, "qwertz", strlen("qwertz") + 1, "BABA");
-  DEBUG_ASSERT(table->n_entries == 3);
-  ht_put(table, "qwertyb", strlen("qwertyb") + 1, "POPO");
-  DEBUG_ASSERT(table->n_entries == 4);
+  char asd[] = "asd";
+  char qwerty[] = "qwerty";
+  char qwertz[] = "qwertz";
+  char qwertb[] = "qwertb";
+  char longstring[] = "iuerbqnwlriujqwherilqwehrilquwehrilqwehrilqweirlur";
 
-  DEBUG_ASSERT(strcmp(ht_get(table, "asd", strlen("asd") + 1), "YAYA") == 0);
-  DEBUG_ASSERT(strcmp(ht_get(table, "qwerty", strlen("qwerty") + 1), "123") ==
-               0);
-  DEBUG_ASSERT(strcmp(ht_get(table, "qwertz", strlen("qwertz") + 1), "BABA") ==
-               0);
-  DEBUG_ASSERT(table->max_entries == 10);
+  ht_put(table, asd, sizeof(asd), "YOYO");
+  DEBUG_ASSERT(table->cur_size == 1);
+  ht_put(table, asd, sizeof(asd), "YAYA");
+  DEBUG_ASSERT(table->cur_size == 1);
+  ht_put(table, qwerty, sizeof(qwerty), "123");
+  DEBUG_ASSERT(table->cur_size == 2);
+  ht_put(table, qwertz, sizeof(qwertz), "BABA");
+  DEBUG_ASSERT(table->cur_size == 3);
+  ht_put(table, qwertb, sizeof(qwertb), "POPO");
+  DEBUG_ASSERT(table->cur_size == 4);
 
-  ht_put(table, "iuerbqnwlriujqwherilqwehrilquwehrilqwehrilqweirlur",
-         strlen("iuerbqnwlriujqwherilqwehrilquwehrilqwehrilqweirlur") + 1,
-         "POPO");
-  DEBUG_ASSERT(table->n_entries == 5);
-  ht_put(table, "", strlen("") + 1,
+  DEBUG_ASSERT(strcmp(ht_get(table, asd, sizeof(asd)), "YAYA") == 0);
+  DEBUG_ASSERT(strcmp(ht_get(table, qwerty, sizeof(qwerty)), "123") == 0);
+  DEBUG_ASSERT(strcmp(ht_get(table, qwertz, sizeof(qwertz)), "BABA") == 0);
+  DEBUG_ASSERT(strcmp(ht_get(table, qwertb, sizeof(qwertb)), "POPO") == 0);
+  DEBUG_ASSERT(table->max_size == 10);
+
+  ht_put(table, longstring, sizeof(longstring), "POPO");
+  DEBUG_ASSERT(table->cur_size == 5);
+  ht_put(table, "", sizeof(""),
          "qwertyblajsdflkjasdkljfklasjdfkjalskdfjlkasjdflkjaslkdfj");
-  DEBUG_ASSERT(table->n_entries == 6);
-  DEBUG_ASSERT(table->max_entries == 20);
+  DEBUG_ASSERT(table->cur_size == 6);
+  DEBUG_ASSERT(table->max_size == 20);
 
-  DEBUG_ASSERT(ht_get(table, "doesnt_exist", strlen("doesnt_exist") + 1) ==
-               NULL);
+  DEBUG_ASSERT(ht_get(table, "doesnt_exist", sizeof("doesnt_exist")) == NULL);
 
   ht_free(table);
 }
