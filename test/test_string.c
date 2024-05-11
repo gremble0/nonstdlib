bb1f3244e94834287692877fa09f8abb76e05ba9
gremble0 <hermanstornes@gmail.com>
2024-05-12 00:09:49 +0200

    Use nullbytes for string tests

--- test/test_string.c
+++ test/test_string.c
@@ -7,9 +7,9 @@
 
 static void test_string_of(void) {
   const char s[] = "Hello, World";
-  string_t *str = string_of(s, sizeof(s) - 1);
+  string_t *str = string_of(s, sizeof(s));
 
-  DEBUG_ASSERT(strlen(s) == str->len);
+  DEBUG_ASSERT(strlen(s) + 1 == str->len);
   DEBUG_ASSERT(strncmp(str->s, s, str->len) == 0);
 
   string_t *str_copy = string_of(str->s, str->len);
@@ -34,14 +34,14 @@ static void test_string_clear(void) {
 static void test_string_set(void) {
   const char s1[] = "Nonstdlib";
   const char s2[] = "Nonstdlib version 2";
-  string_t *str = string_of(s1, sizeof(s1) - 1);
+  string_t *str = string_of(s1, sizeof(s1));
 
-  string_set(str, s2, sizeof(s2) - 1);
+  string_set(str, s2, sizeof(s2));
   DEBUG_ASSERT(strncmp(str->s, s2, str->len) == 0);
 
   string_clear(str);
-  string_set(str, s1, sizeof(s1) - 1);
-  DEBUG_ASSERT(strlen(s1) == str->len);
+  string_set(str, s1, sizeof(s1));
+  DEBUG_ASSERT(strlen(s1) + 1 == str->len);
   DEBUG_ASSERT(strncmp(str->s, s1, str->len) == 0);
 
   string_free(str);
@@ -54,11 +54,11 @@ static void test_string_compare(void) {
   const char s4[] = "N";
   const char s5[] = "N";
 
-  string_t *str1 = string_of(s1, sizeof(s1) - 1);
-  string_t *str2 = string_of(s2, sizeof(s2) - 1);
-  string_t *str3 = string_of(s3, sizeof(s3) - 1);
-  string_t *str4 = string_of(s4, sizeof(s4) - 1);
-  string_t *str5 = string_of(s5, sizeof(s5) - 1);
+  string_t *str1 = string_of(s1, sizeof(s1));
+  string_t *str2 = string_of(s2, sizeof(s2));
+  string_t *str3 = string_of(s3, sizeof(s3));
+  string_t *str4 = string_of(s4, sizeof(s4));
+  string_t *str5 = string_of(s5, sizeof(s5));
 
   DEBUG_ASSERT(string_compare(str1, str1) == 0);
   DEBUG_ASSERT(string_compare(str1, str2) == 1);
